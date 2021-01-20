c3fec0cbbc9697477c385e7df219cb47f25ac0f1
Pedro Gonçalves <pedro@unimestre.com>
2021-01-20 14:59:44 -0300

    exemplo concluído

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('#waves').wavify({
+        height: (window.innerHeight * .43),
+        bones: 6,
+        amplitude: 32,
+        color: '#111',
+        speed: .25
+    });
+
+});
